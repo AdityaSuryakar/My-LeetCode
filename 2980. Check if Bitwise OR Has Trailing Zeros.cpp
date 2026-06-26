2052b297609db5f9974d273fc2257e8c4ae0b71e
Aditya Suryakar <suryakaraditya@gmail.com>
2026-06-26 23:45:26 +0530

    Create 2980. Check if Bitwise OR Has Trailing Zeros.cpp

---
+++ 2980. Check if Bitwise OR Has Trailing Zeros.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool hasTrailingZeros(vector<int>& nums) {
+        int n = nums.size(); 
+        int i = 0; 
+        while (i < n) {
+            int j = 0; 
+            while (j < n) {
+                if (i != j && (nums[i] | nums[j]) % 2 == 0) {
+                    return true; 
+                }
+                j++; 
+            }
+            i++; 
+        }
+        
+        return false; // Return false if no such pair is found
+    }
+};
+
+
