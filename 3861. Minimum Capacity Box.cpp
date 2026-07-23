06444256e2bc3f681514116284a481575d283ca8
Aditya Suryakar <suryakaraditya@gmail.com>
2026-07-23 22:50:41 +0530

    Create 3861. Minimum Capacity Box.cpp

---
+++ 3861. Minimum Capacity Box.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& capacity, int itemSize) {
+        int minCap = INT_MAX;
+        int index = -1;
+
+        for(int i = 0; i < capacity.size(); i++){
+            if(capacity[i] >= itemSize){
+                if(capacity[i] < minCap){
+                    minCap = capacity[i];
+                    index = i;
+                }
+            }
+        }
+
+        return index;
+    }
+};
