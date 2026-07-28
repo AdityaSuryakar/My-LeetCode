a62951f3cd590bd0b1a4450e23240bb75d130a66
Aditya Suryakar <suryakaraditya@gmail.com>
2026-07-28 22:21:12 +0530

    Create 3925. Concatenate Array With Reverse.cpp

---
+++ 3925. Concatenate Array With Reverse.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] concatWithReverse(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+
+        int j = 2 * n - 1;
+
+        for (int i = 0; i < n; i++, j--) {
+            ans[i] = nums[i];
+            ans[j] = nums[i];
+        }
+
+        return ans;
+    }
+}
